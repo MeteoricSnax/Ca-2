7d2ef961fcd2c9d45290800c222abe4a7eaccdca
Albert Vandel <albertvandel@yahoo.dk>
2018-10-03 14:09:44 +0200

    added corsfilters

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -30,6 +30,8 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(exceptions.PersonNotFoundExceptionMapper.class);
+        resources.add(rest.CorsRequestFilter.class);
+        resources.add(rest.CorsResponseFilter.class);
         resources.add(rest.PersonResource.class);
     }
     
